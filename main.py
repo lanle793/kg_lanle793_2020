11fd251b33a14a4005301bdb0d83d5b3e9d717cd
Lan Le <lanle793@gmail.com>
2020-03-19 21:57:02 -0400

    Update main.py
    Read input arguements and print result output after running oneToOneMapping function

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
-class Solution:
-    def oneToOneMapping(self, s1: str, s2: str) -> bool:
-        # dictionary to store mappings
-        mapDict = {}
 
-        if (len(s2) < len(s1)):
+import sys
+
+
+
+def oneToOneMapping(s1: str, s2: str) -> bool:
+    if (len(s2) < len(s1)):
+        return False
+    
+    # dictionary to store mappings
+    mapDict = {}
+
+    numMapping = len(s1)
+    
+    # iterate over 2 strings
+    for index in range(numMapping):
+        key = s1[index]
+        value = s2[index]
+        if mapDict.get(key) is None:
+            mapDict[key] = value
+        elif mapDict[key] != value:
             return False
+    
+    return True
 
-        numMapping = len(s1)
-        
-        # iterate over 2 strings
-        for index in range(numMapping):
-            key = s1[index]
-            value = s2[index]
-            if mapDict[key] is None:
-                mapDict[key] = value
-            elif mapDict[key] != value:
-                return False
-        
-        return True
+
+if __name__ == '__main__':
+    # read string inputs
+    s1 = sys.argv[1]
+    s2 = sys.argv[2]
+
+    # print result output
+    if oneToOneMapping(s1, s2):
+        print('true')
+    else:
+        print('false')
