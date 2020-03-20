b3b545c64c93614dfc8ad5860b3f1bca6c9c55d9
Lan Le <lanle793@gmail.com>
2020-03-19 21:06:22 -0400

    Implement oneToOneMapping function

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 class Solution:
-    def oneToOneMap(self, s1: str, s2: str) -> bool:
-        return False
+    def oneToOneMapping(self, s1: str, s2: str) -> bool:
+        # dictionary to store mappings
+        mapDict = {}
+
+        if (len(s2) < len(s1)):
+            return False
+
+        numMapping = len(s1)
+        
+        # iterate over 2 strings
+        for index in range(numMapping):
+            key = s1[index]
+            value = s2[index]
+            if mapDict[key] is None:
+                mapDict[key] = value
+            elif mapDict[key] != value:
+                return False
+        
+        return True
